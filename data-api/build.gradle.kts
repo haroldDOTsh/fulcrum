776cf23c6eb4bc413812535f6f484a6b5d736a33
🧀 Harold <55301634+ZECHEESELORD@users.noreply.github.com>
2025-07-11 14:51:45 -0400

    feat: Add Mockito dependency

--- data-api/build.gradle.kts
+++ data-api/build.gradle.kts
@@ -8,6 +8,9 @@ dependencies {
     implementation("com.google.code.gson:gson:2.11.0")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
     implementation("org.mongodb:mongodb-driver-sync:4.11.1")
+
+    //mockito
+    testImplementation("org.mockito:mockito-core:5.6.0")
 }
 
 tasks.test {
