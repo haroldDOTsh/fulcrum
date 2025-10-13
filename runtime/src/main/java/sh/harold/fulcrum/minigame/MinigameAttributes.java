24d2cc384cf227831a75b6da7ea2095db92dcd10
🧀 Harold <55301634+ZECHEESELORD@users.noreply.github.com>
2025-10-13 09:19:27 -0400

    have engine handle deaths/eliminations

--- runtime/src/main/java/sh/harold/fulcrum/minigame/MinigameAttributes.java
+++ runtime/src/main/java/sh/harold/fulcrum/minigame/MinigameAttributes.java
@@ -7,6 +7,7 @@ public final class MinigameAttributes {
     public static final String MATCH_COMPLETE = "minigame.match.complete";
     public static final String SLOT_ID = "minigame.slot.id";
     public static final String SLOT_METADATA = "minigame.slot.metadata";
+    public static final String MATCH_ENVIRONMENT = "minigame.match.environment";
     private MinigameAttributes() {
     }
 }
